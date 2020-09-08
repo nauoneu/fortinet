a3da71ab0c45cbf6e01f55c3be9129055095c185
tueno <tueno@fortinet.com>
2020-09-08 16:45:32 +0900

    [Change] create md5 list

--- malshare_getlist.py
+++ malshare_getlist.py
@@ -10,19 +10,23 @@ import datetime
 import sys
 from pprint import pprint
 
-#with open('list.json') as json_file:
-#    data = json.load(json_file)
-
 # global vars
 api_key = "14b204a8ff4fdc3e6f0a7a0aee38446eb2d560c10d76bae2e8980043d5d628af"
 url = "https://malshare.com/api.php"
 getlist = {'action': 'getlist', 'api_key': api_key}
-
+folder = datetime.date.today()
+path = f"/var/www/html/files/{folder}"
 
 def download_hashlist():
     try:
         r = requests.get(url, params = getlist)
         data = r.json()
+        os.makedirs(path)
+        md5list = open(f"{path}/list", "w")
+        for p in data:
+            md5hash = p['md5']
+            md5list.write(f"{md5hash}\n")
+        md5list.close()
 
     except requests.exceptions.RequestException as e:
         print(e, file=sys.stderr)
@@ -32,11 +36,7 @@ def download_hashlist():
 
 def download_samples():
     hashlist = download_hashlist()
-    folder = datetime.date.today()
-    path = f"files/{folder}"
-    os.makedirs(path)
-    with open(os.path.join(path, list), mode = "wb") as fh:
-        fh.write(hashlist)
+#    md5list = open(f"{path}/list", "w")
 
     for p in hashlist:
         md5hash = p['md5']
@@ -46,6 +46,10 @@ def download_samples():
 
         with open(os.path.join(path, md5hash), mode = "wb") as fh:
             fh.write(sample)
+
+#        md5list.write(f"{md5hash}\n")
+
+#    md5list.close()
 
 if __name__ == "__main__":
     download_samples()
